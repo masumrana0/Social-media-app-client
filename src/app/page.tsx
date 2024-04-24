a21c9208b46edf01716c86b408bd6dcdbd492716
Masum Rana <masumrana446267@gmail.com>
2024-04-24 10:33:49 +0600

    weather added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,7 @@
 const HomePage = () => {
-  // const [isLogged, setIsLogged] = useState(false);
-
-  // useEffect(() => {
-  //   setIsLogged(isLoggedIn());
-  // }, []);
-
-  return (
-    <div>
-      {/* <DesktopNavbar /> */}
-      {/* <h3>This is home page</h3>
-      <LoadingSpinner type="regular" />
-      <Button className="bg-sky-800" size="md">
-        Default
-      </Button> */}
-    </div>
-  );
+  return <div>
+    
+  </div>;
 };
 
 export default HomePage;
